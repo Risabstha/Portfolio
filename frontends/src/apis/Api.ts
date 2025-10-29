de6407d1ca953be631e4dcd331bd28886b969f21
Risab Shrestha <risabshrestha50@gmail.com>
2025-10-29 17:54:35 +0545

    apis

---
+++ frontends/src/apis/Api.ts
@@ -0,0 +1,19 @@
+const baseUrl = "http://localhost:5001/api";
+
+interface maildata {
+    username: string;
+    email: string;
+    subject: string;
+    message: string;
+  }
+
+const mailApi =  async (mailData: maildata ) => {
+    const response = await fetch(`${baseUrl}/mail/send`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(mailData),
+    });
+    return response.json();
+  };
+  
+export default mailApi;
